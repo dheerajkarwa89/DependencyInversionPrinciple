d80da3f3e59b3d1ce289391e1dfc1420f494156f
Dheeraj <dheerajkarwa89@gmail.com>
2024-10-13 21:11:39 +0530

    Add project files.

---
+++ After.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversionPrinciple
+{
+    public interface IDataStore
+    {
+        void Save(string data);
+    }
+
+    public class _Database : IDataStore
+    {
+        public void Save(string data)
+        { 
+            //save to database
+        }
+    }
+
+    public class _FileStore : IDataStore
+    {
+        public void Save(string data)
+        {
+            //save to file
+        }
+    }
+
+    public class _DataManager
+    {
+        private IDataStore _store;
+
+        public _DataManager(IDataStore store)
+        {
+            _store = store;
+        }
+
+        public void Save(string data)
+        {
+            _store.Save(data);
+        }
+    }
+}
---
+++ Before.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversionPrinciple
+{
+    public class Database
+    {
+        public void Save(string data)
+        { 
+            //save to database
+        }
+    }
+
+    public class DataManager
+    {
+        private Database db = new Database();
+
+        public void Save(string data)
+        {
+            db.Save(data);
+        }
+    }
+
+}
